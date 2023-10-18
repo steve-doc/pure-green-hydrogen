c00a3dc755d68c33c87a84d2a09d0573631f6588
Steve Docherty <stevedocherty1@gmail.com>
2023-10-18 10:19:19 +0100

    Added email acknowledgement modal

--- assets/scripts/sendEmail.js
+++ assets/scripts/sendEmail.js
@@ -1,5 +1,8 @@
+
 function sendMail(contactForm) {
     emailjs.send("service_0a3s8xf","template_szeqs67", {
+        "to_email": "sdoc67@gmail.com",
+        "website": "PureGH2",
         "from_name": contactForm.name.value,
         "from_email": contactForm.email.value,
         "from_tel": contactForm.tel.value,
@@ -7,6 +10,7 @@ function sendMail(contactForm) {
     })
     .then(
         function(response) {
+            contactForm.reset();
             console.log("SUCCESS", response);
         },
         function(error) {
