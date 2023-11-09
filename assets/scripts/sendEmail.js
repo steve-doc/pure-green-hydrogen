9b883dc2529272464a26ac7ae6d4ede694a4f892
Steve Docherty <stevedocherty1@gmail.com>
2023-11-09 17:15:12 +0000

    updated image files and changed email form destination

--- assets/scripts/sendEmail.js
+++ assets/scripts/sendEmail.js
@@ -1,7 +1,7 @@
 
 function sendMail(contactForm) {
     emailjs.send("service_0a3s8xf","template_szeqs67", {
-        "to_email": "sdoc67@gmail.com",
+        "to_email": "edna.eames@gmail.com",
         "website": "PureGH2",
         "from_name": contactForm.name.value,
         "from_email": contactForm.email.value,
